2f22d7fb85a6f86c568c6400a47315e7645d4bd9
xaota <xaota@users.noreply.github.com>
2017-12-16 13:56:13 +0300

    Открытие фрейма с аукционом

--- integration/avito.js
+++ integration/avito.js
@@ -20,11 +20,18 @@ function integrate() {
       var web3     = new Web3(provider);
       window.web3 = web3;
 
+      window.cb = (e, r) => {
+        console.log('e', JSON.stringify(e));
+        console.log('r', JSON.stringify(r));
+      }
+
       console.log(web3);
 
-    var style = {
-      btn: 'background: rgb(252, 194, 38);display: inline-block;font-size: 16px;padding: 0px 11px;line-height: 25px;border-radius: 4px;cursor:pointer',
-      blueButtonClasses: 'header-button-add-item-2cgWl button-root-1U-0K button-root_size-m-3mjxJ button-root_design-primary-eZ5Gi'
+      var style = {
+        btn: 'background: rgb(252, 194, 38);display: inline-block;font-size: 16px;padding: 0px 11px;line-height: 25px;border-radius: 4px;cursor:pointer',
+        blueButtonClasses: 'header-button-add-item-2cgWl button-root-1U-0K button-root_size-m-3mjxJ button-root_design-primary-eZ5Gi',
+        iframe: 'opacity:0.1;position: absolute;width: 0%;height: 100%;z-index: 1000;background: #fff;right: 0;box-shadow: -10px 0 26px rgba(0,0,0,0.5);',
+        iframeCloseButton: 'opacity:0.1;position: absolute;right: 0%;height: 56px;z-index: 99999999999999;background: #343a40;color: #FFF;padding-left: 12px;line-height: 55px;font-size: 24px;font-weight: bold;cursor: pointer;padding-right: 12px;'
     };
 
     var price = document.querySelector('.title-info-title-text');
@@ -33,7 +40,11 @@ function integrate() {
 
 
     doc.on('click', '#sysByEth', function(e){
-      var btn = e.target;
+      var btn = $(e.target);
+      var oldBg = btn.css('background');
+      btn
+          .css('background', '#777')
+          .text('Ожидание...');
 
       console.log('эфир');
 
@@ -68,7 +79,11 @@ function integrate() {
         value: web3.toWei('0.1', 'ether')
       }
       // web3.eth.personal.unlockAccount('0xdCAA18564716478C6a4Bb2b05B85A95dE106781A', '1')
-      web3.eth.sendTransaction(tx);
+      web3.eth.sendTransaction(tx, (e, r) => {
+        btn
+          .css('background', 'rgb(167, 221, 119)')
+          .text('Успешно');
+      });
 
       console.log(btn, 'Дёргай етериум');
     });
@@ -77,8 +92,45 @@ function integrate() {
 
     self.after('<div class="header-button-wrapper-iwCvN openAuction" style="margin-right:12px;background:#f00"><a href="javascript:void(0)" class="' + style.blueButtonClasses + '" style="background:#f00">Аукционы</a></div>');
 
+    doc.on('click', '#closeAuction', function(e){
+        doc.find('#auctionIframe')
+           .animate({
+               opacity: 0.25,
+               width: "0%"
+           }, 500, function(){
+            doc.find('#auctionIframe').remove();
+        });
+
+        doc.find('#closeAuction')
+           .animate({
+               opacity: 0.25,
+               right: "0%"
+           }, 500, function(){
+            doc.find('#closeAuction').remove();
+            doc.css('overflow', '');
+        });
+    });
+
     doc.on('click', '.openAuction', function(e){
-      console.log('Открыть аукцион');
+        doc.append('<iframe id="auctionIframe" src="' + apiUrl + '/index.html?q=' + +new Date() + '#lot" style="' + style.iframe + '"></iframe>');
+        doc.append('<div id="closeAuction" style="' + style.iframeCloseButton + '" title="Закрыть">✖</div>');
+        doc.css('overflow', 'hidden');
+
+        doc.find('#auctionIframe')
+           .animate({
+               opacity: 1,
+               width: "80%"
+           }, 500, function(){
+        });
+
+        doc.find('#closeAuction')
+           .animate({
+               opacity: 1,
+               right: "80%"
+           }, 500, function(){
+        });
+
+        console.log('Открыть аукцион');
     });
   }
 };
