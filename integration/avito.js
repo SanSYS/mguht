f3efa98e7414af0775f5718ffd492273f9226bf6
xaota <xaota@users.noreply.github.com>
2017-12-16 13:44:03 +0300

    Web3 send transaction

--- integration/avito.js
+++ integration/avito.js
@@ -5,7 +5,7 @@ integrate();
 function integrate() {
   let repo   = 'https://localhost:443/mguht/';
   let rpcURL = 'https://localhost:443/eth'; // 'http://5.178.85.201:8545'; // 'https://localhost:443/eth';
-  let apiUrl = 'https://cbe97c02.ngrok.io/A';
+  let apiUrl = 'https://2558d94c.ngrok.io';
 
   init();
 
@@ -14,12 +14,13 @@ function integrate() {
 
       var doc = $(document.body);
 
-      // console.dir(Web3);
+      console.dir(Web3);
 
-      // var provider = new Web3.providers.HttpProvider(rpcURL);
-      // var web3     = new Web3(provider);
+      var provider = new Web3.providers.HttpProvider(rpcURL);
+      var web3     = new Web3(provider);
+      window.web3 = web3;
 
-      // console.log(web3);
+      console.log(web3);
 
     var style = {
       btn: 'background: rgb(252, 194, 38);display: inline-block;font-size: 16px;padding: 0px 11px;line-height: 25px;border-radius: 4px;cursor:pointer',
@@ -36,15 +37,38 @@ function integrate() {
 
       console.log('эфир');
 
-      // var coinbase = web3.eth.coinbase;
-      // var balance = web3.eth.getBalance(coinbase);
-      // var account = web3.eth.accounts[0];
+      let accounts = ["0x09374b1c383f75be7cb0c7693c0cad7ff93a3ef8", "0xdcaa18564716478c6a4bb2b05b85a95de106781a"];
+      let password = '1';
 
-      // console.dir(coinbase);
-      // console.dir(balance);
-      // console.dir(web3.eth.accounts);
+      function transaction(from, to, amount) {
+        let tx = {
+          amount,
+          from,
+          to,
+          pass: password
+        };
 
+        // https://cbe97c02.ngrok.io/Auction/send
+        $.post(apiUrl + 'Auction/send', tx).then(r => console.dir(r))
 
+      }
+      // {"amount":1, "from":"0x09374b1c383f75be7cb0c7693c0cad7ff93a3ef8", "to":"0x9E592A53506e0711F462406729Ce59164D911F06", "pass":"1"}
+
+      var coinbase = web3.eth.coinbase;
+      var balance = web3.eth.getBalance(coinbase);
+      // var accounts = web3.eth.accounts;
+
+      console.dir(coinbase);
+      console.dir(balance);
+      console.dir(web3.eth.accounts);
+
+      let tx = {
+        from: web3.eth.accounts[0], // '0xdCAA18564716478C6a4Bb2b05B85A95dE106781A',
+        to: web3.eth.accounts[1], // '0x09374B1c383f75be7Cb0C7693C0cAD7FF93A3Ef8',
+        value: web3.toWei('0.1', 'ether')
+      }
+      // web3.eth.personal.unlockAccount('0xdCAA18564716478C6a4Bb2b05B85A95dE106781A', '1')
+      web3.eth.sendTransaction(tx);
 
       console.log(btn, 'Дёргай етериум');
     });
